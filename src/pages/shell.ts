36ca753f525f33aa16f839a87eb1d5fa44ebe9fb
Matthew James Davis <davismj@foursails.co>
2018-12-12 18:11:53 +0900

    [1.01] Add a router

--- src/pages/shell.ts
+++ src/pages/shell.ts
@@ -1,2 +1,15 @@
+import { PLATFORM } from 'aurelia-framework';
+import { RouterConfiguration, Router } from 'aurelia-router';
+
 export class ShellViewModel {
+
+  private router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    this.router = router;
+    config.map([
+      { route: '', redirect: 'contact' },
+      { route: 'contact', moduleId: PLATFORM.moduleName('pages/contact-list'), name: 'contact-list', title: 'Contacts', nav: true }
+    ]);
+  }
 }
