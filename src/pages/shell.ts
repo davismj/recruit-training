ce632b548b0e1dde372b5b40361ea025027001d3
Matthew James Davis <davismj@foursails.co>
2018-12-14 18:53:20 +0900

    [2.04] Translate the text assets

--- src/pages/shell.ts
+++ src/pages/shell.ts
@@ -1,16 +1,28 @@
-import { PLATFORM } from 'aurelia-framework';
+import { inject, PLATFORM } from 'aurelia-framework';
 import { RouterConfiguration, Router } from 'aurelia-router';
+import { I18N } from 'aurelia-i18n';
 
+@inject(I18N)
 export class ShellViewModel {
 
+  locale: string;
+
   private router: Router;
+
+  constructor(private i18n: I18N) {
+    this.locale = i18n.getLocale();
+  }
 
   configureRouter(config: RouterConfiguration, router: Router) {
     this.router = router;
     config.map([
       { route: '', redirect: 'contact' },
-      { route: 'contact', moduleId: PLATFORM.moduleName('pages/contact-list'), name: 'contact-list', title: 'Contacts', nav: true },
+      { route: 'contact', moduleId: PLATFORM.moduleName('pages/contact-list'), name: 'contact-list', title: 'shell.CONTACT_LIST', nav: true },
       { route: 'contact/:id', moduleId: PLATFORM.moduleName('pages/contact-list'), name: 'contact-detail' },
     ]);
+  }
+
+  updateLocale() {
+    this.i18n.setLocale(this.locale);
   }
 }
