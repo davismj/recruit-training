365632e60223805ada6fbc987868b4688f831693
Matthew James Davis <davismj@foursails.co>
2018-12-14 15:50:44 +0900

    [2.02] Basic validation

--- src/pages/contact-list.ts
+++ src/pages/contact-list.ts
@@ -34,15 +34,35 @@ export class ContactListViewModel {
   }
 
   save() {
-    const { id, name, phone, email } = this.contact;
-    this.contactService.saveContact({ id, name, phone, email });
-    this.router.navigateToRoute('contact-list');
+    if (this.isValid) {
+      const { id, name, phone, email } = this.contact;
+      this.contactService.saveContact({ id, name, phone, email });
+      this.router.navigateToRoute('contact-list');
+    }
   }
 
   remove(contact: IContact) {
     this.contactService.deleteContact(contact);
     if (this.contact.id == contact.id) {
       this.router.navigateToRoute('contact-list');
+    }
+  }
+
+  isPhoneNumber(str) {
+    const phoneRe = /^\d{3}[-\s]?\d{4}[-\s]?\d{4}$/i;
+    return !str || phoneRe.test(str);
+  }
+
+  isEmail(str) {
+    const emailRe = /^.+@.+\.\w{2,3}(?:\.{\w2,3})?/i
+    return !str || emailRe.test(str);
+  }
+
+  get isValid() {
+    if (this.contact) {
+      const { name, phone, email } = this.contact;
+      const { isPhoneNumber, isEmail } = this;
+      return name && isPhoneNumber(phone) && isEmail(email);
     }
   }
 }
